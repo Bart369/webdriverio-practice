00bdd8881225f9f5f6643bf2925f5f85055d91d1
bart369 <bartinpeguero@gmail.com>
2019-06-25 23:03:07 -0400

    changed Sync value in WDIO to false

--- tests/webdriverUniversityTest.js
+++ tests/webdriverUniversityTest.js
@@ -1,21 +1,21 @@
-describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
-	it("check that the contact us button opens the contact us page", function(done) {
+describe("Verify whether webdriveruniversity links on homepage work correctly", function () {
+	it("check that the contact us button opens the contact us page", function (done) {
 		return browser
 			.setViewportSize({
 				width: 1200,
 				height: 800
 			})
 			.url('http://www.webdriveruniversity.com/')
-			.getTitle().then(function(title) {
+			.getTitle().then(function (title) {
 				console.log('Title is: ' + title);
 			})
 			.click("#contact-us")
 			.pause(3000)
 	});
 
-	it("check that the login button opens the login portal page", function(done) {
+	it("check that the login button opens the login portal page", function (done) {
 		return browser
-			.url('https://webdriveruniversity.com')
+			.url('http://www.webdriveruniversity.com/')
 			.click('#login-portal')
 			.getTitle().then(function (title) {
 				console.log('Title is: ' + title);
