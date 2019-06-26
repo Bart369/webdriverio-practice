e9da4f64b2daf8e03ddc56f9156cfd957eb230b0
bart369 <bartinpeguero@gmail.com>
2019-06-25 22:52:15 -0400

    removed original two files

--- tests/loginPortalTest.js
+++
@@ -1,9 +0,0 @@
-browser
-    .url('https://webdriveruniversity.com')
-    //.setValue('#search_form_input_homepage', 'WebdriverIO')
-    .click('#login-portal')
-    .getTitle().then(function(title) {
-        console.log('Title is: ' + title);
-    })
-
-
