90c6db001747f36fef08be443c992661d6039ee6
bart369 <bartinpeguero@gmail.com>
2019-07-03 01:49:43 -0400

    chai assertions test

---
+++ tests/chai.js
@@ -0,0 +1,13 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+var actual = 1;
+var expected = 2;
+
+// Below are the 3 differect formats we can use to make chai assertions
+assert.equal(actual, expected);
+expect(actual).to.equal(expected);
+actual.should.equal(expected);
+
+// Run the following to only test these tests "npm test -- --spec/tests/chai.js"
