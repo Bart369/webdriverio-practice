577608c37455ee25f3d95ca142bc8ec5a4ea4583
bart369 <bartinpeguero@gmail.com>
2019-07-11 02:07:51 -0400

    Add chai assertions

--- tests/chai.js
+++ tests/chai.js
@@ -2,7 +2,7 @@ var assert = require('chai').assert; //this Node's standard assert functionality
 var expect = require('chai').expect;
 var should = require('chai').should();
 
-var actual = 1;
+var actual = 2;
 var expected = 2;
 
 // Below are the 3 differect formats we can use to make chai assertions
--- tests/webdriverUniversityTest.js
+++ tests/webdriverUniversityTest.js
@@ -1,4 +1,7 @@
-var assert = require('assert'); //nodejs assertions
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+// var assert = require('assert'); *** nodejs assertions which we will not use. We use the chai assertion aboves
 
 describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
 	it("check that the contact us button opens the contact us page", function(done) {
@@ -8,7 +11,8 @@ describe("Verify whether webdriveruniversity links on homepage work correctly", 
 			})
 			browser.url('/') 		// We stored the URL in the baseUrl variable in the config file and we are keeping our code dry by not repeaing the url
 			var title = browser.getTitle();
-			assert.strictEqual(title, 'WebDriverUniversity.com') //strict compares the two variables, fails if they are not equal.
+			// assert.strictEqual(title, 'WebDriverUniversity.com') *** this node assertion got replaced by chai line 15 *** strict compares the two variables, fails if they are not equal.
+			expect(title).to.equal('WebDriverUniversity.com');
 			console.log('Title is: ' + title);
 			browser.click("#contact-us")
 			browser.pause(3000)
@@ -18,7 +22,8 @@ describe("Verify whether webdriveruniversity links on homepage work correctly", 
 			browser.url('/')  // "/" parameter will use the baseUrl set in the config file.
 			browser.click('#login-portal')
 			var title = browser.getTitle();
-			assert.strictEqual(title, 'WebDriverUniversity.com')
+			// assert.strictEqual(title, 'WebDriverUniversity.com') *** this node assertion got replaced by chai line 26
+			title.should.equal('WebDriverUniversity.com');
 			console.log('Title is: ' + title);
 			browser.pause(3000)
 	})
