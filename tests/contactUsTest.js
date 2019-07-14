2b88f01985572fcaf458cddee6005c0672559fb1
bart369 <bartinpeguero@gmail.com>
2019-07-14 13:34:44 -0400

    steps for each it command

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -8,23 +8,31 @@ beforeEach(function () {
 
 describe('Test Contact Us form WebdriverUni.', function () {
     it('Should be able to submit a successful submission via contact us form', function (done) {
-        [name='first_name']
-        [name='last_name']
-        [name='email']
-        [name='message']
-        [type = 'submit']
-   
+        browser.setValue("[name='first_name']", 'Joe');
+        browser.setValue("[name='last_name']", 'Blogs');
+        browser.setValue("[name='email']", 'joe_Blogs@mail.com');
+        browser.setValue("[name='message']", "How much does X cost?");
+        browser.click("[type = 'submit']");
+    });
+    
+    // Using the Webdriverio setValue method. setValue sends a sequence of key strokes to an element(clears value before).
+    // We also use Webdriverio's click command
+    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+        browser.setValue("[name='first_name']", 'Joe');
+        browser.setValue("[name='last_name']", 'Blogs');
+        browser.setValue("[name='email']", 'joe_Blogs@mail.com');
+        browser.click("[type = 'submit']");
     });
 
     it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-   
+        browser.setValue("[name='first_name']", 'Joe');
+        browser.setValue("[name='email']", 'joe_Blogs@mail.com');
+        browser.click("[type = 'submit']");
     });
 
     it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-   
-    });
-
-    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-   
+         browser.setValue("[name='first_name']", 'Joe');
+         browser.setValue("[name='last_name']", 'Blogs');
+         browser.click("[type = 'submit']");
     });
 });
